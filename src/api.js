055113b91021f416b85a413b0a4a47eac09b00ab
Mustafa Khan <157674883+mm5khan@users.noreply.github.com>
2025-10-06 11:27:47 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3000/api";
+const API_BASE = import.meta.env.VITE_API_URL + "/api";;
 
 export async function fetchTasks() {
   const res = await fetch(`${API_BASE}/tasks`);
